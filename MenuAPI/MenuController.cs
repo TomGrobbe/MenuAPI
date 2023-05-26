03fca928491ca225659a2c3ee06aa924a672f792
Sky <skylarplayz348@gmail.com>
2023-05-25 22:24:20 -0500

    Add a texture dictionary for custom headers as well as able to define image in said dictionary

--- MenuAPI/MenuController.cs
+++ MenuAPI/MenuController.cs
@@ -12,8 +12,8 @@ namespace MenuAPI
         public static List<Menu> Menus { get; protected set; } = new List<Menu>();
         internal static HashSet<Menu> VisibleMenus { get; } = new HashSet<Menu>();
 #if FIVEM
-        public const string _texture_dict = "commonmenu";
-        public const string _header_texture = "interaction_bgd";
+        public const string _texture_dict = "menutextures";
+        public static string _header_texture = "main";
 #endif
 #if REDM
         public const string _texture_dict = "menu_textures";
@@ -24,6 +24,7 @@ namespace MenuAPI
 #if FIVEM
             "commonmenu",
             "commonmenutu",
+            "menutextures",
             "mpleaderboard",
             "mphud",
             "mpshopsale",
